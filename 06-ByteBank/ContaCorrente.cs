7b1401947dd3742af0f8fb603630efc5af480f9c
Kemoel Miranda <kemoel.amorim@gmail.com>
2022-04-20 08:18:43 -0300

    Get e Set

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        public double Saldo {
+            get { 
+                return _saldo;
+            }
+            set { 
+                if(value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            } }
+        
+        public bool Sacar(double valor)
+        {
+            if(this._saldo >= valor){
+                this._saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if(valor <= 0)
+            {
+                Console.WriteLine($"O valor :{valor} não é permitido");
+            }
+            else
+            {
+                this._saldo += valor;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente conta)
+        {
+            if(this.Sacar(valor))
+            {
+                conta.Depositar(valor);
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ 06-ByteBank/Programa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto _06_ByteBank");
+
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Kemoel";
+            cliente.CPF = "000.000.000-00";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
